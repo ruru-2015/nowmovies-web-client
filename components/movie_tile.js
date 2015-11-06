bb3e87993447218069ffaba858323b42721f5870
Sulu Fiti <email": null>
2015-11-06 16:31:27 +1300

    add example of a click function that uses state

--- components/movie_tile.js
+++ components/movie_tile.js
@@ -2,16 +2,31 @@ import React from 'react'
 import Poster from './poster'
 import MovieInfo from './movie_info'
 
-class MovieTile extends React.Component{
-  render(){
+export class MovieTile extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { isClicked: false }
+  }
+
+  render() {
+
+    console.log('this', this)
+
     return(
-      <div className="movieTile">
-        <h1> {this.props.movie.title} </h1>
+      <div onClick={this.clickHandler.bind(this)} className="movieTile">
+        {this.state.isClicked ? <div>"Clicked"</div> : null}
+        <h1> {this.props.movie.Addtitle} </h1>
         <Poster poster={this.props.movie.poster} />
         <MovieInfo data={this.props.movie} />
       </div>
     )
   }
+
+  clickHandler() {
+    console.log('clicked')
+    this.setState({isClicked: !this.state.isClicked })
+  }
+
 }
 
 export default MovieTile
